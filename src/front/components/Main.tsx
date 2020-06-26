008b9e6792f22285fba9bdaef7ebc3b3832e5894
Mateusz <mateusz@Mateuszs-MacBook-Air.local>
2020-06-26 12:12:43 +0200

    code format

--- src/front/components/Main.tsx
+++ src/front/components/Main.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import {Issue} from '../components/Issue';
+import React from "react";
+import { Issue } from "../components/Issue";
 
-export function MainPage(props : any) {
-  return <div className="container">
-  <h2>Issue Tracker</h2>
-  <div className="row">
-  {props.items.map((element:any) => {
-    return <Issue item = {element}/>
-  })}
-  </div></div>
+export function MainPage(props: any) {
+  return (
+    <div className="container">
+      <h2>Issue Tracker</h2>
+      <div className="row">
+        {props.items.map((element: any) => {
+          return <Issue item={element} />;
+        })}
+      </div>
+    </div>
+  );
 }
